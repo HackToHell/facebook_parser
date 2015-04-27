ae246d5016fddfb0f467915dbadc0a45f07ef2c3
Gowtham <hacktohell@yahoo.com>
2015-04-27 10:36:20 +0530

    Better JSON formatting

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,16 +6,22 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.jsoup.nodes.Document;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 import org.json.simple.JSONValue;
+
+import javax.json.Json;
+import javax.json.JsonValue;
+import javax.json.stream.JsonGenerator;
+import javax.json.stream.JsonGeneratorFactory;
+
 
 public class Main {
 
@@ -30,6 +36,11 @@ public class Main {
         TreeMap<String, Integer> sorted_map = new TreeMap<String, Integer>(bvc);
         int i = 0;
         String tmp;
+        Map<String, Object> properties = new HashMap<String, Object>(1);
+        properties.put(JsonGenerator.PRETTY_PRINTING, true);
+        JsonGeneratorFactory jgf = Json.createGeneratorFactory(properties);
+
+
 
 
         File input = new File("C:/Users/Gowtham/Downloads/fb/html/messages.htm");
@@ -42,6 +53,8 @@ public class Main {
         }
         FileWriter fw = new FileWriter(output);
         BufferedWriter bw = new BufferedWriter(fw);
+        JsonGenerator gf = jgf.createGenerator(bw);
+        gf.writeStartArray();
         try {
             // Document tester = Jsoup.parse(test);
             // System.out.println(tester.select(".thread").select(".user").first().text());
@@ -63,16 +76,17 @@ public class Main {
                     }
                 }
                 map.put(tmp, message.size());
+                gf.writeStartObject().write("Name", tmp).write("message", message.size()).writeEnd();
 
             }
         } catch (Exception e) {
             System.err.print(e);
         }
+        gf.writeEnd();
+        gf.close();
         System.out.println(map);
         sorted_map.putAll(map);
         System.out.println(sorted_map);
-        bw.write(JSONValue.toJSONString(sorted_map));
-        bw.close();
 
 
     }
